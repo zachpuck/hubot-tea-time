27b4b787ac1459b2c9102e28db4bef69de6715d1
zachpuck <zachpuckett@gmail.com>
2017-08-30 23:04:47 -0700

    fix comments

--- src/tea-time.js
+++ src/tea-time.js
@@ -1,17 +1,17 @@
-# Description
-#   A hubot package for daily tea time!
-#
-# Configuration:
-#   LIST_OF_ENV_VARS_TO_SET
-#
-# Commands:
-#   hubot tea time - <what the respond trigger does>
-#
-# Notes:
-#   <optional notes required for the script>
-#
-# Author:
-#   zach puckett <zachpuckett@gmail.com>
+//  Description
+//    A hubot package for daily tea time!
+// 
+//  Configuration:
+//    LIST_OF_ENV_VARS_TO_SET
+// 
+//  Commands:
+//    hubot tea time - <what the respond trigger does>
+// 
+//  Notes:
+//    <optional notes required for the script>
+// 
+//  Author:
+//    zach puckett <zachpuckett@gmail.com>
 'use strict';
 
 module.exports = function(robot) {
